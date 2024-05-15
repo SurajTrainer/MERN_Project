6e0cf912323fb3c9dccde64bdf187b9148941120
Suraj Singh <playerking2244@gmail.com>
2024-05-15 15:22:04 +0530

    adding data on product component

--- Frontend/src/product/Product.jsx
+++ Frontend/src/product/Product.jsx
@@ -8,6 +8,11 @@ const Product = () => {
   return (
     <>
     <Navbar/>
+    <div className='container product_data'>
+      <h2>This is all productsa list.. <span style={{color:"#FF1493"}}>special : )</span> </h2>
+      <p>Quickly design and customize responsive most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, </p>
+      <button type="button" class="btn btn-outline-danger" style={{margin:'12px 41%'}}>Danger</button>
+    </div>
     <div className='d-flex flex-wrap container product_data'>
         {list.map((item) => (
           <Card key={item.id} item={item}/>
