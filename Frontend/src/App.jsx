b5ec898b42697e561d2b28c4c0a064d86cd821ed
Suraj Singh <playerking2244@gmail.com>
2024-05-15 13:57:32 +0530

    reacr-router-dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './home/Home'
+import Product from './components/Product'
 
 
 const App = () => {
   return (
    <>
+          
         <Routes>
-          <Route path = '/' element = {<Home/>} />
-          {/* <Route path = '/product' element = {<Product/>} /> */}
+          <Route path = '/' element = {<Home/>}/>
+          <Route path = '/product' element = {<Product/>} />
         </Routes>
+         
    </>
    
   )
