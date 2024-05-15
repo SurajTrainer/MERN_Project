6bc2697f10faadbbad57e05e26ce2cfe137f47a5
Suraj Singh <playerking2244@gmail.com>
2024-05-15 14:49:00 +0530

    json update

--- Frontend/src/product/Product.jsx
+++ Frontend/src/product/Product.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import list from '../../public/list.json'
+import Card from '../components/Card'
 
 const Product = () => {
   return (
     <>
     <Navbar/>
-     <h1 className='container' style={{marginTop:"140px"}}>products</h1>
+    <div className='d-flex flex-wrap container product_data'>
+        {list.map((item) => (
+          <Card key={item.id} item={item}/>
+        ))}
+        </div>
+
      <div className="product_footer">
      <Footer/>
      </div>
