027da9ac00acb0e9e2fb809ef4418bf376f81e38
Suraj Singh <playerking2244@gmail.com>
2024-05-16 17:02:46 +0530

    updating back button from products

--- Frontend/src/product/Product.jsx
+++ Frontend/src/product/Product.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import list from '../../public/list.json'
 import Card from '../components/Card'
+import { Link } from 'react-router-dom'
 
 const Product = () => {
   return (
@@ -11,7 +12,10 @@ const Product = () => {
     <div className='container product_data'>
       <h2>This is all productsa list.. <span style={{color:"#FF1493"}}>special : )</span> </h2>
       <p>Quickly design and customize responsive most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, </p>
-      <button type="button" class="btn btn-outline-danger" style={{margin:'12px 41%'}}>Danger</button>
+
+      <Link to='/'>
+      <button type="button" class="btn btn-outline-danger" style={{margin:'12px 41%'}}>Back</button>
+      </Link>
     </div>
     <div className='d-flex flex-wrap container product_data'>
         {list.map((item) => (
