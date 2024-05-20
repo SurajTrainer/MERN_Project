b6b0ea3de1acaf34d78cac6a01da5d1723300ed9
Suraj Singh <playerking2244@gmail.com>
2024-05-20 11:14:33 +0530

    mongoose connect

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,18 @@ app.get('/', (req, res) => {
   res.send(`<h1>Hello Mr. Suraj Singh Bisht</h1>`)
 })
 
+
+// connect to mongoDB
+
+try {
+    mongoose.connect(URI, {
+      useNewUrlParser : true,
+      useUnifiedTopology : true
+    } )
+} catch (error) {
+  
+}
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
