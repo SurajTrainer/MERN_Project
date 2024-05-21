5838af72356c5265d78c1f7eb5317f6e722ef780
Suraj Singh <playerking2244@gmail.com>
2024-05-21 11:47:07 +0530

    login user data storing

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -31,3 +31,26 @@ export const signup = async(req, res) => {
         res.status(500).json({message : " internal server errror.."})
     }
 }
+
+
+export const login = async(req , res) => {
+  try {
+      const {fullname , email} = req.body
+      const user = await User.findOne({email})
+      const isMatch = bcryptjs.compare(password , user.password)
+      if (!user || !isMatch) {
+          return res.status(400).json({message : " invalid username and password.."})
+      }else{
+          res.status(200).json({message : "Login successfully..." , 
+              user : {
+                  _id : user._id,
+                  fullname : user.fullname,
+                  email : user.email
+              }
+          })
+      }
+  } catch (error) {
+        console.log("error is :" , error);
+        res.status(500).json({message : "Server error from internal..."})
+  }
+}
