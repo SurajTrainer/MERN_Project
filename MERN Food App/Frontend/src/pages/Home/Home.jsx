f474358601c0d32dfdb1a013607e747a87fe9794
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-05-28 17:31:10 +0530

    Home.jsx

--- MERN Food App/Frontend/src/pages/Home/Home.jsx
+++ MERN Food App/Frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../component/Header/Header'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
 
 const Home = () => {
   return (
   <>
-    
+    <Header/>
+    <ExploreMenu/>
   </>
   )
 }
 
 export default Home
+
