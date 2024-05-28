c5420ccd0b811c2e724d815b3036a04fa0eac103
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-05-28 17:33:53 +0530

    Header

---
+++ MERN Food App/Frontend/src/component/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+   <>
+   <div className="header">
+
+        <div className="header-bg p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+    <div className="header-text col-lg-6 px-0">
+      <h1 className="display-4 fst-italic">Food Making is my destination.</h1>
+      <p className="lead my-3">I want to surve my  taisty food with everyone have and want to explore myself by this way. please try to taist mt hand's made food..</p>
+      <p className="lead mb-0"><a href="#" className="text-body-emphasis fw-bold">Continue reading...</a></p>
+    </div>
+  </div>
+   </div>
+   </>
+  )
+}
+
+export default Header
